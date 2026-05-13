30aefcf9be5f243b065b686108cad95c8cae52bd
FrancoFern <francosimonfernandezrubio@gmail.com>
2026-05-12 23:41:35 -0300

    subida de SQL de creacion de la DB (se borro con el ultimo commit de frontend)

---
+++ Backend/CreateDB.sql
@@ -0,0 +1,82 @@
+CREATE TABLE `Gestor` (
+  `id_Gestor` integer PRIMARY KEY,
+  `nombre` varchar(255),
+  `email` varchar(255) UNIQUE,
+  `password` varchar(255),
+  `telefono` varchar(255),
+  `id_rol` integer
+);
+
+CREATE TABLE `Clientes` (
+  `id_usuario` integer PRIMARY KEY,
+  `nombre` varchar(255),
+  `email` varchar(255) UNIQUE,
+  `password` varchar(255),
+  `dni` varchar(255) UNIQUE,
+  `telefono` varchar(255)
+);
+
+CREATE TABLE `Roles` (
+  `id_rol` integer PRIMARY KEY,
+  `nombre_rol` varchar(255)
+);
+
+CREATE TABLE `Propiedad` (
+  `id_propiedad` integer PRIMARY KEY,
+  `titulo` varchar(255),
+  `descripcion` text,
+  `precio` decimal,
+  `habitaciones` integer,
+  `baños` integer,
+  `superficie` float,
+  `acepta_mascotas` boolean,
+  `id_tipo` integer,
+  `id_estado` integer,
+  `id_gestor` integer
+);
+
+CREATE TABLE `TipoPropiedad` (
+  `id_tipo` integer PRIMARY KEY,
+  `nombre_tipo` varchar(255)
+);
+
+CREATE TABLE `PropiedadEstado` (
+  `id_estado` integer PRIMARY KEY,
+  `nombre_estado` varchar(255)
+);
+
+CREATE TABLE `Contractos` (
+  `id_contrato` integer PRIMARY KEY,
+  `id_inquilino_comprador` integer,
+  `fecha_inicio` date,
+  `fecha_fin` date,
+  `monto` decimal,
+  `url_documento_firmado` varchar(255),
+  `id_propiedad` integer
+);
+
+CREATE TABLE `Favoritos` (
+  `id_favorito` integer PRIMARY KEY,
+  `id_propiedad` integer,
+  `id_usuario` integer
+);
+
+ALTER TABLE `Gestor` COMMENT = 'Centraliza a dueños, agentes e interesados';
+
+ALTER TABLE `Clientes` COMMENT = 'Centraliza a dueños, agentes e interesados';
+
+ALTER TABLE `Gestor` ADD CONSTRAINT `Tiene_Rol` FOREIGN KEY (`id_rol`) REFERENCES `Roles` (`id_rol`);
+
+ALTER TABLE `Propiedad` ADD CONSTRAINT `Gestiona` FOREIGN KEY (`id_gestor`) REFERENCES `Gestor` (`id_Gestor`);
+
+ALTER TABLE `Propiedad` ADD CONSTRAINT `Es_Tipo` FOREIGN KEY (`id_tipo`) REFERENCES `TipoPropiedad` (`id_tipo`);
+
+ALTER TABLE `Propiedad` ADD CONSTRAINT `Tiene_Estado` FOREIGN KEY (`id_estado`) REFERENCES `PropiedadEstado` (`id_estado`);
+
+ALTER TABLE `Contractos` ADD CONSTRAINT `Tiene_Propiedad` FOREIGN KEY (`id_propiedad`) REFERENCES `Propiedad` (`id_propiedad`);
+
+ALTER TABLE `Contractos` ADD CONSTRAINT `Puede_ver` FOREIGN KEY (`id_inquilino_comprador`) REFERENCES `Clientes` (`id_usuario`);
+
+ALTER TABLE `Favoritos` ADD CONSTRAINT `Marca` FOREIGN KEY (`id_usuario`) REFERENCES `Clientes` (`id_usuario`);
+
+ALTER TABLE `Favoritos` ADD CONSTRAINT `Es_marcada` FOREIGN KEY (`id_propiedad`) REFERENCES `Propiedad` (`id_propiedad`);
