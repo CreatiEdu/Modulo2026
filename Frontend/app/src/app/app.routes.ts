06738bf6642507ab02689cca0c31b28118c50e4b
DOLOFER21 <doloresfr029@gmail.com>
2026-05-29 17:28:32 -0300

    creación pag de contacto

--- Frontend/app/src/app/app.routes.ts
+++ Frontend/app/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { QuienesSomosComponent } from './pages/quienes-somos/quienes-somos';
 import { Home } from './pages/home/home';
 import { Login } from './pages/login/login';
+import { Contacto } from './pages/contacto/contacto';
 import { DashboardUsuario } from './pages/dashboard-usuario/dashboard-usuario';
 import { DashboardAdmin } from './pages/dashboard-admin/dashboard-admin';
 import { Pagina404 } from '../pages/pagina404/pagina404';
@@ -12,6 +13,7 @@ export const routes: Routes = [
   { path: 'login', component: Login },
   { path: 'dashboard-usuario', component: DashboardUsuario },
   { path: 'dashboard-admin', component: DashboardAdmin },
+  { path: 'contacto', component: Contacto },
 
   // agregado de rutas
   { path: '', redirectTo: '/home', pathMatch: 'full' },
