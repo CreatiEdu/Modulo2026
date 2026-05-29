1f3acd4afef81bb7cdc665cd8b1143c22f4a7e43
alegariglio <alejandrogariglio1986@gmail.com>
2026-05-28 23:38:23 -0300

    creacion de rutas por defecto y 404

--- Frontend/app/src/app/app.routes.ts
+++ Frontend/app/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { Home } from './pages/home/home';
 import { Login } from './pages/login/login';
 import { DashboardUsuario } from './pages/dashboard-usuario/dashboard-usuario';
 import { DashboardAdmin } from './pages/dashboard-admin/dashboard-admin';
+import { Pagina404 } from '../pages/pagina404/pagina404';
 
 export const routes: Routes = [
   { path: '', component: Home },
@@ -11,5 +12,9 @@ export const routes: Routes = [
   { path: 'login', component: Login },
   { path: 'dashboard-usuario', component: DashboardUsuario },
   { path: 'dashboard-admin', component: DashboardAdmin },
+
+  // agregado de rutas
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: Pagina404 }
 ];
 
