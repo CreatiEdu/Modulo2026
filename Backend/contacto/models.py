6e874410aa135fe830485ba561989b8bac097c5c
MatiGranjero <73409869+MatiGranjero@users.noreply.github.com>
2026-06-02 17:52:30 -0300

    Actualizacion de API REST contacto

--- Backend/contacto/models.py
+++ Backend/contacto/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class MensajeContacto(models.Model):
+class Contacto(models.Model):
     nombre = models.CharField(max_length=100, verbose_name="Nombre Completo")
     email = models.EmailField(verbose_name="Correo Electronico")
     telefono = models.CharField(max_length=20, blank=True, null=True, verbose_name="Numero Telefonico")
@@ -9,4 +9,4 @@ class MensajeContacto(models.Model):
     asunto= models.CharField(max_length=100, verbose_name="Asunto")
 
     def __str__(self):
-        return f'Mensaje de {self.nombre} - {self.fecha_envio.strftime("%d/%m/%Y %H:%M")}'
+        return self.fecha_envio.strftime("%d/%m/%Y %H:%M")
