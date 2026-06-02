246a3537fa176014e59afc0ad16bce34459cbeec
MatiGranjero <73409869+MatiGranjero@users.noreply.github.com>
2026-06-02 17:37:26 -0300

    Feature: API REST Contacto añadido

---
+++ Backend/contacto/urls.py
@@ -0,0 +1,8 @@
+
+from rest_framework.routers import DefaultRouter
+from .views import ContactoViewSet
+
+router = DefaultRouter()
+router.register(r'contacto', ContactoViewSet)
+
+urlpatterns = router.urls
---
+++ Backend/contacto/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+from rest_framework import viewsets
+from .models import Contacto
+from .serializers import ContactoSerializer
+class ContactoViewSet(viewsets.ModelViewSet):
+    queryset = Contacto.objects.all().order_by("fecha_envio")
+    serializer_class = ContactoSerializer
